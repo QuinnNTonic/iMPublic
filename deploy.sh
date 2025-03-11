b8c8544726e265ad4babd3c2f76306a74eb731cd
Quinn <33108110+QuinnNTonic@users.noreply.github.com>
2025-03-11 08:25:57 -0400

    feat: Added the steps to install node to the deploy shell script.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
+#install node & npm
+sudo apt-get install -y curl
+curl -fsSL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh
+sudo -E bash nodesource_setup.sh
+sudo apt-get install -y nodejs
+node -v
+
 npm run deploy:test
